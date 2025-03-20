17adbac1be0d77308ee33354849b84ec535d6646
SoU-240517 <fsttamaprot.broadside024@passinbox.com>
2025-03-20 19:30:27 +0900

    キャッシュ削除

--- coloring/gradient.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def compute_gradient(shape):
-    x, y = np.indices(shape)
-    normalized_distance = np.sqrt((x - shape[0]/2)**2 + (y - shape[1]/2)**2) / np.sqrt((shape[0]/2)**2 + (shape[1]/2)**2)
-    return normalized_distance
--- main.py
+++
@@ -1,7 +0,0 @@
-from ui.main_window import MainWindow
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()  # Tkinter のルートウィンドウを作成
-    app = MainWindow(root)  # MainWindow のインスタンスを作成
-    root.mainloop()  # Tkinter のイベントループを開始
